dd1665cd193c6dd35a5563376d745ae551d5156f
Manel-Dahora <69764804+Manel-Dahora@users.noreply.github.com>
2021-11-23 18:51:46 -0300

    update

---
+++ Assets/ManualInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace BioPunk
+{
+    public class ManualInput : MonoBehaviour
+    {
+        private CharacterControl _characterControl;
+
+        private void Awake()
+        {
+            _characterControl = this.gameObject.GetComponent<CharacterControl>();
+        }
+
+        private void Update()
+        {
+            _characterControl.MoveRight = VirtualInputManager.Instance.MoveRight;
+            _characterControl.MoveLeft = VirtualInputManager.Instance.MoveLeft;
+            _characterControl.Jump = VirtualInputManager.Instance.Jump;
+        }
+    }
+}
